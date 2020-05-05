db564519671b5653aa4346c52b82701b25a3a86b
chen <chenshaobo272@gmail.com>
2020-05-05 23:52:04 +0800

    fix: run

--- server/src/magic_style/run.sh
+++ server/src/magic_style/run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo -e "\033[1;32m initializing databases \033[0m"
+echo -e "\e[1;31m 1. initializing databases \e[0m"
 while :; do
     if python3 manage.py migrate; then
         break
@@ -8,7 +8,8 @@ while :; do
     sleep 1
 done
 
-echo -e "\033[1;32m server start \033[0m"
+python3 manage.py create_superuser
 
+echo -e "\e[1;31m 2. server start \e[0m"
 gunicorn magic_style.wsgi:application -c gunicorn_conf.py
-echo -e "\033[1;32m finished! \033[0m"
+echo -e "\e[1;31m 3. finished! \e[0m"
