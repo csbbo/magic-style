2c5a63e09f60432036601ab756ed34eb4506b72b
chen <chenshaobo272@gmail.com>
2020-05-05 21:51:47 +0800

    fix: rpc

--- server/src/magic_style/rpc/gen_grpc_code.py
+++ server/src/magic_style/rpc/gen_grpc_code.py
@@ -1,4 +1,6 @@
 from grpc_tools import protoc
 
-protoc.main(('', '-I./', '--python_out=.', '--grpc_python_out=.', './style_migrate/style_migrate.proto'))
-protoc.main(('', '-I./', '--python_out=.', '--grpc_python_out=.', './anime_style/anime_style.proto'))
+protoc.main(('', '-I./', '--python_out=.', '--grpc_python_out=.', './rpc/style_migrate/style_migrate.proto'))
+protoc.main(('', '-I./', '--python_out=.', '--grpc_python_out=.', './rpc/anime_style/anime_style.proto'))
+
+# python rpc/gen_grpc_code.py
